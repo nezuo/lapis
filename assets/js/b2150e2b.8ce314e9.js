1fbd9f791b49aafdf65666d932b3b168063b7f0f
nezuo <hinckleytmicah@gmail.com>
2022-12-03 23:22:03 -0800

    Built and published by Moonwave

---
+++ assets/js/b2150e2b.8ce314e9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[872],{63891:s=>{s.exports=JSON.parse('{"sourceUrl":"https://www.github.com/nezuo/lapis/blob/master","baseUrl":"/lapis/","classOrder":[],"apiCategories":[]}')}}]);
