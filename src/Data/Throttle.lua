a4cbdb30963cbad47d0cabfdf18aa277829bb7b1
nezuo <hinckleytmicah@gmail.com>
2023-07-19 18:07:29 -0700

    Add new throttle queue Removed SUPER_SPEED tests and Clock Removed UnixTimestampMillis mock Fixed :save and :close promises resolving with a value Improved tests

--- src/Data/Throttle.lua
+++ src/Data/Throttle.lua
@@ -1,4 +1,30 @@
+local RunService = game:GetService("RunService")
+
 local Promise = require(script.Parent.Parent.Parent.Promise)
+
+local function updateAsync(request)
+	return Promise.new(function(resolve)
+		local result, transformed
+
+		local ok, err = pcall(function()
+			request.dataStore:UpdateAsync(request.key, function(...)
+				result, transformed = request.transform(...)
+
+				if result == "succeed" then
+					return transformed
+				else
+					return nil
+				end
+			end)
+		end)
+
+		if not ok then
+			resolve("retry", err)
+		else
+			resolve(result, transformed)
+		end
+	end)
+end
 
 local Throttle = {}
 Throttle.__index = Throttle
@@ -6,6 +32,7 @@ Throttle.__index = Throttle
 function Throttle.new(config)
 	return setmetatable({
 		config = config,
+		queue = {},
 	}, Throttle)
 end
 
@@ -13,91 +40,69 @@ function Throttle:getUpdateAsyncBudget()
 	return self.config:get("dataStoreService"):GetRequestBudgetForRequestType(Enum.DataStoreRequestType.UpdateAsync)
 end
 
-function Throttle:retry(attempts, delay, callback)
-	for attempt = 1, attempts do
-		local result, value = callback()
+function Throttle:start()
+	RunService.PostSimulation:Connect(function()
+		for index = #self.queue, 1, -1 do
+			local request = self.queue[index]
 
-		if result == "succeed" then
-			return true, value
-		elseif result == "fail" then
-			return false, value
-		elseif attempt == attempts then
-			return false, `DataStoreFailure({value})`
+			if request.attempts == 0 then
+				table.remove(self.queue, index)
+			end
 		end
 
-		if self.config:get("showRetryWarnings") then
-			warn(`DataStore operation failed. Retrying...\nError: {value}`)
-		end
-
-		if delay ~= nil then
-			Promise.delay(delay):expect()
-		end
-	end
-
-	error("unreachable")
-end
-
-function Throttle:startQueue()
-	while #self.queue > 0 do
-		local request = table.remove(self.queue, 1)
-
-		local ok, value = self:retry(request.attempts, request.delay, function()
-			while self:getUpdateAsyncBudget() == 0 do
-				task.wait()
+		for _, request in self.queue do
+			if self:getUpdateAsyncBudget() == 0 then
+				break
 			end
 
-			local result, transformed
+			if request.promise ~= nil then
+				continue
+			end
 
-			local updateOk, err = pcall(function()
-				request.dataStore:UpdateAsync(request.key, function(...)
-					result, transformed = request.transform(...)
+			local promise = updateAsync(request):andThen(function(result, value)
+				if result == "succeed" then
+					request.attempts = 0
+					request.resolve(value)
+				elseif result == "fail" then
+					request.attempts = 0
+					request.reject(`DataStoreFailure({value})`)
+				elseif result == "retry" then
+					request.attempts -= 1
 
-					if result == "succeed" then
-						return transformed
+					if request.attempts == 0 then
+						request.reject(`DataStoreFailure({value})`)
 					else
-						return nil
+						if self.config:get("showRetryWarnings") then
+							warn(`DataStore operation failed. Retrying...\nError: {value}`)
+						end
+
+						task.wait(request.retryDelay)
 					end
-				end)
+				else
+					error("unreachable")
+				end
+
+				request.promise = nil
 			end)
 
-			if not updateOk then
-				return "retry", err
+			if promise:getStatus() == Promise.Status.Started then
+				request.promise = promise
 			end
-
-			return result, transformed
-		end)
-
-		if ok then
-			request.resolve(value)
-		else
-			request.reject(value)
 		end
-	end
-
-	self.queue = nil
+	end)
 end
 
 function Throttle:updateAsync(dataStore, key, transform, retryAttempts, retryDelay)
 	return Promise.new(function(resolve, reject)
-		local request = {
+		table.insert(self.queue, {
 			dataStore = dataStore,
 			key = key,
 			transform = transform,
 			attempts = retryAttempts,
-			delay = retryDelay,
+			retryDelay = retryDelay,
 			resolve = resolve,
 			reject = reject,
-		}
-
-		if self.queue == nil then
-			self.queue = { request }
-
-			task.spawn(function()
-				self:startQueue()
-			end)
-		else
-			table.insert(self.queue, request)
-		end
+		})
 	end)
 end
 
